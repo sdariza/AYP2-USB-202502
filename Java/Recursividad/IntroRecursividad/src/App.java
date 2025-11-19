5bbb1f2a6f09a480a010bc8aea494cecb0920f28
Sebastian David Ariza Coll <39573068+sdariza@users.noreply.github.com>
2025-11-18 20:37:40 -0500

    feat: ✨add Recursion

---
+++ Java/Recursividad/IntroRecursividad/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Method myMethod = new Method();
+        System.out.println("4! = "+ myMethod.fact(4));
+        System.out.println("99999! = "+ myMethod.fact(99999));
+    }
+}
---
+++ Java/Recursividad/IntroRecursividad/src/Method.java
@@ -0,0 +1,9 @@
+public class Method {
+    public int fact(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+    
+}
